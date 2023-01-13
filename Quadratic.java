2ad465f003632d1c312072d869119c8a0ba62271
Tyler Stuckey <tylerwstuckey@gmail.com>
2023-01-13 12:19:59 -0600

    added no solution to quadratic

--- Quadratic.java
+++ Quadratic.java
@@ -1,5 +1,25 @@
+import javax.naming.NameNotFoundException;
+
 public class Quadratic {
     public static void main(String[] args) {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int c = Integer.parseInt(args[2]);
+        double bsq = Math.pow(b, 2);
+      
+        double discrim = (bsq - (4 * a * c));
+         double result1 =  (-b + Math.sqrt(bsq - discrim)) / (2 * a);
+        double result2 =  (-b - Math.sqrt(bsq - discrim)) / (2 * a);
+       
+        if (a == 0) {
+            double noa = (-c / b);
+            System.out.println(noa);
+        } else if (discrim < 0) {
+            System.out.println("no solution");
+        } else {
+            System.out.println(result1 + "," + result2);
+        }
+
         
     }
 }
